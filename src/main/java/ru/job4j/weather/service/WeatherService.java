f84e406cd925294734f7c014d9344d834cf1a598
Aleksey Krylov <elsite2015@yandex.ru>
2021-05-06 17:00:07 +0400

    2. Spring boot Webflux [#477081]

---
+++ src/main/java/ru/job4j/weather/service/WeatherService.java
@@ -0,0 +1,45 @@
+package ru.job4j.weather.service;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.job4j.weather.model.Weather;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Service
+public class WeatherService {
+
+    private final Map<Integer, Weather> weathers = new ConcurrentHashMap<>();
+    {
+        weathers.put(1, new Weather(1, "Msc", 20));
+        weathers.put(2, new Weather(2, "SPb", 15));
+        weathers.put(3, new Weather(3, "Bryansk", 15));
+        weathers.put(4, new Weather(4, "Smolensk", 15));
+        weathers.put(5, new Weather(5, "Kiev", 15));
+        weathers.put(6, new Weather(6, "Minsk", 15));
+    }
+
+    public Mono<Weather> findById(Integer id) {
+        return Mono.justOrEmpty(weathers.get(id));
+    }
+
+    public Flux<Weather> all() {
+        return Flux.fromIterable(weathers.values());
+    }
+
+    public Mono<List<Weather>> findByGreatThanTemperature(int temperature) {
+        return Flux.fromIterable(weathers.values())
+                .filter(o -> o.getTemperature() > temperature)
+                .collectList();
+    }
+
+    public Mono<List<Weather>> findByHighestTemperature() {
+        Integer tempMAX = Flux.fromIterable (weathers.values())
+                .map(Weather::getTemperature)
+                .reduce(Integer::max).toProcessor().block();
+        return Flux.fromIterable(weathers.values()).filter(w -> w.getTemperature() == tempMAX).collect(Collectors.toList());
+    }
+}
